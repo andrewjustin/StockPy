848150f0f5e663716cd5004aa711b2f5a038f96b
Andrew Justin <76486321+andrewjustin@users.noreply.github.com>
2022-02-04 22:05:04 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,76 +21,41 @@ def download_stock_data(symbol, interval='daily'):
     return stock_data
 
 
-def get_indicator_indices(dates, start_date=None, end_date=None):
-    """ Get indices """
+class Stock(object):
+    def __init__(self, symbol, start_date=None, end_date=None, interval='daily'):
+        self.stock_df = download_stock_data(str(symbol), interval=str(interval))
+        if start_date is not None:
+            start_index = np.where(self.dates() == str(start_date))[0]
+            if len(start_index) == 0:
+                raise ValueError(f"{str(start_date)} is not in the array of dates, try a different date.")
+            else:
+                self.start_index = start_index[0]
+        else:
+            self.start_index = 0
 
-    if start_date is not None:
-        start_index = np.where(dates == start_date)[0][0]
-    else:
-        start_index = 0
+        if end_date is not None:
+            end_index = np.where(self.dates() == str(end_date))[0]
+            if len(end_index) == 0:
+                raise ValueError(f"{str(end_date)} is not in the array of dates, try a different date.")
+            else:
+                self.end_index = end_index[0]
+        else:
+            self.end_index = len(self.dates())-1
 
-    if end_date is not None:
-        end_index = np.where(dates == end_date)[0][0]
-    else:
-        end_index = len(dates)
+    def dates(self):
+        return self.stock_df['Date'].values
 
-    return start_index, end_index
+    def opens(self):
+        return self.stock_df['Open'].values
 
+    def highs(self):
+        return self.stock_df['High'].values
 
-def split_dataframe(stock_df, start_date=None, end_date=None):
-    """ Split stock df """
-    dates = stock_df['Date'].values
-    opens = stock_df['Open'].values
-    highs = stock_df['High'].values
-    lows = stock_df['Low'].values
-    closes = stock_df['Close'].values
-    volumes = stock_df['Volume'].values
+    def lows(self):
+        return self.stock_df['Low'].values
 
-    if start_date is not None and end_date is not None:
-        opens = slice_data_by_date(dates, opens, start_date, end_date)
-        highs = slice_data_by_date(dates, highs, start_date, end_date)
-        lows = slice_data_by_date(dates, lows, start_date, end_date)
-        closes = slice_data_by_date(dates, closes, start_date, end_date)
-        volumes = slice_data_by_date(dates, volumes, start_date, end_date)
-        dates = select_date_range(dates, start_date, end_date)
-        return dates, opens, highs, lows, closes, volumes
-    else:
-        return dates, opens, highs, lows, closes, volumes
+    def closes(self):
+        return self.stock_df['Close'].values
 
-
-def select_date_range(dates, start_date, end_date):
-    """ Slice dates """
-
-    dates = np.array(dates, dtype='datetime64[D]')
-    start_date = np.array([start_date], dtype='datetime64[D]')[0]
-    end_date = np.array([end_date], dtype='datetime64[D]')[0]
-
-    start_date_index = np.where(dates == start_date)[0]
-    if len(start_date_index) == 0:
-        raise ValueError(f"{start_date} is not in the array of dates, try a different date.")
-    end_date_index = np.where(dates == end_date)[0]
-    if len(end_date_index) == 0:
-        raise ValueError(f"{end_date} is not in the array of dates, try a different date.")
-
-    dates = dates[start_date_index[0]:end_date_index[0]+1]
-
-    return dates
-
-
-def slice_data_by_date(dates, data, start_date, end_date):
-    """ Slice data by date """
-
-    dates = np.array(dates, dtype='datetime64[D]')
-    start_date = np.array([start_date], dtype='datetime64[D]')[0]
-    end_date = np.array([end_date], dtype='datetime64[D]')[0]
-
-    start_date_index = np.where(dates == start_date)[0]
-    if len(start_date_index) == 0:
-        raise ValueError(f"{start_date} is not in the array of dates, try a different date.")
-    end_date_index = np.where(dates == end_date)[0]
-    if len(end_date_index) == 0:
-        raise ValueError(f"{end_date} is not in the array of dates, try a different date.")
-
-    data = data[start_date_index[0]:end_date_index[0]+1]
-
-    return data
+    def volumes(self):
+        return self.stock_df['Volume'].values
