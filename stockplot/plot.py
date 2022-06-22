e896cb9234b4a6f599269acd025d039a880108ba
Andrew Justin <76486321+andrewjustin@users.noreply.github.com>
2022-06-22 18:10:24 -0500

    general updates

--- stockplot/plot.py
+++ stockplot/plot.py
@@ -1,11 +1,30 @@
+"""
+Functions for plotting stock data.
+
+Last updated: 2/4/2022 10:09 PM CST
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
+from stockplot.plotting_utils import shift_indicator
 import indicators
 
 
 def average_directional_index(stock_object, ax=None, period=14, **plot_kwargs):
-    """ ADX """
+    """
+    Plots the average directional index (ADX).
+
+    ax: matplotlib.axes instance, default: None
+        Axis where the ADX will be plotted.
+    period: int, default: 14
+        Period for the smoothed averages in the ADX formula.
+    stock_object: utils.Stock object
+        Object that contains all data for the stock.
+    **plot_kwargs: matplotlib.lines.Line2D properties
+
+    For more info on the ADX, see: https://www.investopedia.com/terms/a/adx.asp
+    """
     adx = indicators.average_directional_index(stock_object, period=period)[period:]
 
     if ax is None:
@@ -15,7 +34,21 @@ def average_directional_index(stock_object, ax=None, period=14, **plot_kwargs):
 
 
 def average_true_range(stock_object, ax=None, period=14, method='simple', **plot_kwargs):
-    """ Calculate ATR """
+    """
+    Plots the average true range (ATR).
+
+    ax: matplotlib.axes instance, default: None
+        Axis where the ADX will be plotted.
+    method: str, default: 'simple'
+        Method for calculating the moving average of the true range. Available options are: 'simple', 'exponential', 'wilder'
+    period: int, default: 14
+        Period used for calculating the moving average of the true range.
+    stock_object: utils.Stock object
+        Object that contains all data for the stock.
+    **plot_kwargs: matplotlib.lines.Line2D properties
+
+    For more info on the ATR, see: https://www.investopedia.com/terms/a/atr.asp
+    """
     atr = indicators.average_true_range(stock_object, period=period, method=method)[period:]
 
     if ax is None:
@@ -28,7 +61,24 @@ def average_true_range(stock_object, ax=None, period=14, method='simple', **plot
 
 def bollinger_bands(stock_object, ax=None, period=20, std_devs=2, upper_band_color='blue',
     lower_band_color='orange', middle_band_color='black', **plot_kwargs):
-    """ Bollinger Bands """
+    """
+    Plots the bollinger bands.
+
+    ax: matplotlib.axes instance, default: None
+        Axis where the bollinger bands will be plotted.
+    lower_band_color: str, default: 'orange'
+    middle_band_color: str, default: 'black'
+    period: int, default: 20
+        Period used for calculating moving averages for the bands.
+    std_devs: int, default: 2
+        Number of standard deviations that the upper and lower bands will be displaced from the middle band.
+    stock_object: utils.Stock object
+        Object that contains all data for the stock.
+    upper_band_color: str, default: 'blue'
+    **plot_kwargs: matplotlib.lines.Line2D properties
+
+    For more info on the bollinger bands, see: https://www.investopedia.com/terms/b/bollingerbands.asp
+    """
     bands = indicators.bollinger_bands(stock_object, period=period, std_devs=std_devs)[:,period:]
 
     if ax is None:
@@ -42,7 +92,22 @@ def bollinger_bands(stock_object, ax=None, period=20, std_devs=2, upper_band_col
 
 
 def candles(stock_object, candle_scale=1.0, ax=None, wick_color='black', up_color='green', down_color='red', **vlines_kwargs):
-    """ Plot candles """
+    """
+    Plots candlesticks.
+
+    ax: matplotlib.axes instance, default: None
+        Axis where the bollinger bands will be plotted.
+    candle_scale: float, default: 1.0
+        Scale factor for the widths of the candles' bodies and wicks.
+    down_color: color string, default: 'red'
+        Color of the bodies for candles where the close price is lower than the open price.
+    stock_object: utils.Stock object
+        Object that contains all data for the stock.
+    up_color: color string, default: 'green'
+        Color of the bodies for candles where the close price is greater than the open price.
+    wick_color: color string, default: 'black'
+    **vlines_kwargs: matplotlib.lines.Line2D properties
+    """
     opens = stock_object.opens()
     highs = stock_object.highs()
     lows = stock_object.lows()
@@ -105,6 +170,13 @@ def exponential_moving_average(stock_object, ax=None, period=50, multiplier_nume
     """ EMA """
     ema = indicators.exponential_moving_average(stock_object, period=period, multiplier_numerator=multiplier_numerator)[period:]
 
+    plotted_days = stock_object.end_index - stock_object.start_index + 1
+    indicator_length = len(ema)
+    shift_needed = plotted_days - indicator_length
+
+    if shift_needed > 0:
+        ema = shift_indicator(ema, shift_needed)
+
     if ax is None:
         plt.plot(ema, label=f'EMA ({period})', **plot_kwargs)
     else:
@@ -123,7 +195,7 @@ def ichimoku_cloud(stock_object, ax=None, conversion_period=9, base_period=26, s
         plt.plot(cloud[0], color=conversion_color, label=f'IC - Conv. Line ({conversion_period})', **plot_kwargs)
         plt.plot(cloud[1], color=base_color, label=f'IC - Base Line ({base_period})', **plot_kwargs)
         plt.plot(cloud[2], color=span_a_color, label=f'IC - Span A ({span_a_offset})', **plot_kwargs)
-        plt.plot(cloud[3], color=span_b_color, label=f'IC - Span B ({span_b_period, span_b_offset})', **plot_kwargs)
+        plt.plot(cloud[3], color=span_b_color, label=f'IC - Span B {span_b_period, span_b_offset}', **plot_kwargs)
         plt.fill_between(timesteps+span_a_offset, cloud[2][span_a_offset:], cloud[3][span_b_offset:], where=cloud[3][span_b_offset:] > cloud[2][span_a_offset:],
             color='red', alpha=0.4)
         plt.fill_between(timesteps+span_a_offset, cloud[2][span_a_offset:], cloud[3][span_b_offset:], where=cloud[3][span_b_offset:] < cloud[2][span_a_offset:],
@@ -132,7 +204,7 @@ def ichimoku_cloud(stock_object, ax=None, conversion_period=9, base_period=26, s
         ax.plot(cloud[0], color=conversion_color, label=f'IC - Conv. Line ({conversion_period})', **plot_kwargs)
         ax.plot(cloud[1], color=base_color, label=f'IC - Base Line ({base_period})', **plot_kwargs)
         ax.plot(cloud[2], color=span_a_color, label=f'IC - Span A ({span_a_offset})', **plot_kwargs)
-        ax.plot(cloud[3], color=span_b_color, label=f'IC - Span B ({span_b_period, span_b_offset})', **plot_kwargs)
+        ax.plot(cloud[3], color=span_b_color, label=f'IC - Span B {span_b_period, span_b_offset}', **plot_kwargs)
         ax.fill_between(timesteps+span_a_offset, cloud[2][span_a_offset:], cloud[3][span_b_offset:], where=cloud[3][span_b_offset:] > cloud[2][span_a_offset:],
             color='red', alpha=0.4)
         ax.fill_between(timesteps+span_a_offset, cloud[2][span_a_offset:], cloud[3][span_b_offset:], where=cloud[3][span_b_offset:] < cloud[2][span_a_offset:],
@@ -210,6 +282,13 @@ def simple_moving_average(stock_object, ax=None, period=50, **plot_kwargs):
     """ SMA """
     sma = indicators.simple_moving_average(stock_object, period=period)
 
+    plotted_days = stock_object.end_index - stock_object.start_index + 1
+    indicator_length = len(sma)
+    shift_needed = plotted_days - indicator_length
+
+    if shift_needed > 0:
+        sma = shift_indicator(sma, shift_needed)
+
     if ax is None:
         plt.plot(sma[period:], label=f'SMA ({period})', **plot_kwargs)
     else:
@@ -257,6 +336,13 @@ def weighted_moving_average(stock_object, ax=None, period=50, **plot_kwargs):
     """ WMA """
     wma = indicators.weighted_moving_average(stock_object, period=period)
 
+    plotted_days = stock_object.end_index - stock_object.start_index + 1
+    indicator_length = len(wma)
+    shift_needed = plotted_days - indicator_length
+
+    if shift_needed > 0:
+        wma = shift_indicator(wma, shift_needed)
+
     if ax is None:
         plt.plot(wma, label=f'WMA ({period})', **plot_kwargs)
     else:
@@ -282,3 +368,94 @@ def xticks(stock_object, period='D', ax=None, **kwargs):
         else:
             ax.set_xticks(unique_index_array)
             ax.set_xticklabels(new_dates, **kwargs)
+
+
+def momentum_convergence(stock_obj, ax=None, period=10, **kwargs):
+    """
+    RSI divergence
+    """
+
+    closes = stock_obj.closes()[stock_obj.start_index:stock_obj.end_index+1]
+    convergence_array = np.empty(len(closes))
+    trend_array = np.empty(len(closes))
+
+    rsi = indicators.relative_strength_index(stock_obj, price='close', period=14)
+
+    for day in range(period + 1, stock_obj.end_index - stock_obj.start_index + 1):
+        change_percent = (closes[day] / closes[day-period] - 1)
+        rsi_change = (rsi[day] / rsi[day - period] - 1)
+        divergence = change_percent * rsi_change * 100
+        convergence_array[day] = divergence
+        trend_array[day] = change_percent * 100
+
+    maximum_num = np.nanmax(abs(convergence_array))
+
+    if ax is None:
+        plt.plot(trend_array, label=f'Trend ({period})', color='black', **kwargs)
+        plt.plot(convergence_array, label=f'Convergence ({period})', color='blue', **kwargs)
+        plt.hlines(xmin=0, xmax=len(trend_array) - 1, y=0, color='black', linewidths=1)
+        plt.ylim(-maximum_num, maximum_num)
+        plt.ylabel('Convergence')
+    else:
+        ax.plot(trend_array, label=f'Trend ({period})', color='black', **kwargs)
+        ax.plot(convergence_array, label=f'Convergence ({period})', color='blue', **kwargs)
+        ax.hlines(xmin=0, xmax=len(trend_array) - 1, y=0, color='black', linewidths=1)
+        ax.set_ylim(-maximum_num, maximum_num)
+        ax.set_ylabel('Convergence')
+
+
+def support_resistance(stock_obj, ax=None, period=10, price_range=0.50, **kwargs):
+    """
+    Plot support and resistance levels
+    """
+
+    highs = stock_obj.highs()[stock_obj.start_index:stock_obj.end_index+1]
+    lows = stock_obj.lows()[stock_obj.start_index:stock_obj.end_index+1]
+
+    critical_levels = np.array([])
+
+    for day in range(period, stock_obj.end_index - stock_obj.start_index):
+        max_high = np.max(highs[day-period:day])
+        min_high = np.min(highs[day-period:day])
+        average_high = np.mean(highs[day-period:day])
+        max_low = np.max(lows[day-period:day])
+        min_low = np.min(lows[day-period:day])
+        average_low = np.mean(lows[day-period:day])
+        if max_high - min_high < price_range:
+            if ax is None:
+                if len(critical_levels) == 0:
+                    critical_levels = np.append(critical_levels, max_high)
+                    plt.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=max_high, color='black', **kwargs)
+                    plt.text(x=stock_obj.end_index - stock_obj.start_index, y=max_high, s=max_high.__format__('.2f'))
+                elif np.min(np.abs(critical_levels - max_high)) > price_range:
+                    critical_levels = np.append(critical_levels, max_high)
+                    plt.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=max_high, color='black', **kwargs)
+                    plt.text(x=stock_obj.end_index - stock_obj.start_index, y=max_high, s=max_high.__format__('.2f'))
+            else:
+                if len(critical_levels) == 0:
+                    critical_levels = np.append(critical_levels, max_high)
+                    ax.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=max_high, color='black', **kwargs)
+                    ax.text(x=stock_obj.end_index - stock_obj.start_index, y=max_high, s=max_high.__format__('.2f'))
+                elif np.min(np.abs(critical_levels - max_high)) > price_range:
+                    critical_levels = np.append(critical_levels, max_high)
+                    ax.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=max_high, color='black', **kwargs)
+                    ax.text(x=stock_obj.end_index - stock_obj.start_index, y=max_high, s=max_high.__format__('.2f'))
+        if max_low - min_low < price_range:
+            if ax is None:
+                if len(critical_levels) == 0:
+                    critical_levels = np.append(critical_levels, min_low)
+                    plt.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=min_low, color='black', **kwargs)
+                    plt.text(x=stock_obj.end_index - stock_obj.start_index, y=min_low, s=min_low.__format__('.2f'))
+                elif np.min(np.abs(critical_levels - min_low)) > price_range:
+                    critical_levels = np.append(critical_levels, min_low)
+                    plt.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=min_low, color='black', **kwargs)
+                    plt.text(x=stock_obj.end_index - stock_obj.start_index, y=min_low, s=min_low.__format__('.2f'))
+            else:
+                if len(critical_levels) == 0:
+                    critical_levels = np.append(critical_levels, min_low)
+                    ax.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=min_low, color='black', **kwargs)
+                    ax.text(x=stock_obj.end_index - stock_obj.start_index, y=min_low, s=min_low.__format__('.2f'))
+                elif np.min(np.abs(critical_levels - min_low)) > price_range:
+                    critical_levels = np.append(critical_levels, min_low)
+                    ax.hlines(xmin=0, xmax=stock_obj.end_index - stock_obj.start_index, y=min_low, color='black', **kwargs)
+                    ax.text(x=stock_obj.end_index - stock_obj.start_index, y=min_low, s=min_low.__format__('.2f'))
